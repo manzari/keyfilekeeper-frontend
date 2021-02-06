7d427674c853dd8503af8ccd9f180510fa575089
manzari <22736528+manzari@users.noreply.github.com>
2021-02-06 01:20:26 +0100

    add links to help page

--- src/components/Help.js
+++ src/components/Help.js
@@ -5,6 +5,11 @@ const Help = (props) => {
     return (
         <Content>
             <h2>Help</h2>
+            <ul>
+                <li><a target="_blank" href={window.apiUrl + "/doc"}>API Documentation</a> (only in dev mode)</li>
+                <li><a target="_blank" href="https://github.com/manzari/keyfilekeeper-server">Backend Source Code</a></li>
+                <li><a target="_blank" href="https://github.com/manzari/keyfilekeeper-frontend">Frontend Source Code</a></li>
+            </ul>
         </Content>
     )
 }
